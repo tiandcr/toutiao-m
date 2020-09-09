2beb811552aacd076cc7507ac54d817d060dcb22
td09 <tiand09@163.com>
2020-09-09 12:53:24 +0800

    init is ok

--- src/main.js
+++ src/main.js
@@ -2,7 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// // 加载vant核心库
+// import Vant from 'vant'
+// // 加载vant全局样式
+// import 'vant/lib/index.css'
+// 加载全局样式
+import './style/index.less'
+// // 注册使用Vant组件库
+// Vue.use(Vant)
+import './plugins/vant'
+//
+import 'amfe-flexible'
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/plugins/vant.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+// 加载vant核心库
+import Vant from 'vant'
+// 加载vant全局样式
+import 'vant/lib/index.css'
+// 注册使用Vant组件库
+Vue.use(Vant)
